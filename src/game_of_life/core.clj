e5d1386ca2c1d4ff4136afa67e371608a3baeb12
clindv <clindv@protonmail.com>
2019-02-03 16:09:48 +0800

    init draw-function from https://raw.githubusercontent.com/clojurebook/ClojureProgramming/master/ch03-game-of-life/src/com/clojurebook/collections/life.clj

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,7 +1,32 @@
 (ns game-of-life.core
   (:gen-class))
 
+(defn draw
+  [w h step cells]
+  (let [state (atom cells)
+        run (atom true)
+        listener (proxy [java.awt.event.WindowAdapter] []
+                   (windowClosing [_] (reset! run false)))
+        pane
+          (doto (proxy [javax.swing.JPanel] []
+                  (paintComponent [^java.awt.Graphics g]
+                    (let [g (doto ^java.awt.Graphics2D (.create g)
+                              (.setColor java.awt.Color/BLACK)
+                              (.fillRect 0 0 (* w 10) (* h 10))
+                              (.setColor java.awt.Color/WHITE))]
+                      (doseq [[x y] @state]
+                        (.fillRect g (inc (* 10 x)) (inc (* 10 y)) 8 8)))))
+            (.setPreferredSize (java.awt.Dimension. (* 10 w) (* 10 h))))]
+    (doto (javax.swing.JFrame. "Game of Life")
+      (.setContentPane pane)
+      (.addWindowListener listener)
+      .pack
+      (.setVisible true))
+    (future (while @run
+              (Thread/sleep 80)
+              (swap! state step)
+              (.repaint pane)))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (draw 100 100 identity #{[15 15] [15 17] [16 16] [15 16]}))
